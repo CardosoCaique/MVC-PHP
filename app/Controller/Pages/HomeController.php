5ee334d535e115102a3b31830a518997337e8a1c
Caíque Cardoso <caiquecardoso.ti@gmail.com>
2022-03-01 22:53:06 -0300

    Criçãod a estrutura

---
+++ app/Controller/Pages/HomeController.php
@@ -0,0 +1,14 @@
+<?php
+    namespace App\Controller\Pages;
+
+    /**
+     * Método responsável por retornar o conteúdo da Home
+     * @return string
+     */
+    class HomeController
+    {
+        public static function getHome()
+        {
+            return "Hello, world";
+        }
+    }
---
+++ app/Controller/Utils/View.php
@@ -0,0 +1,15 @@
+<?php
+    namespace App\Utils;
+
+    /**
+     * Método responsável por retornar o conteúdo de uma view
+     * @param string $view
+     * @return string
+     */
+    class View
+    {
+        public static function render($view)
+        {
+
+        }
+    }
